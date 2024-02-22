ebadc78a14c4146dcec50aff7f3c233f28a36533
LOHAN2000 <160059524+LOHAN2000@users.noreply.github.com>
2024-02-22 23:47:22 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#boton-excusa").addEventListener("click", () => {
+    document.querySelector("#excusa-generador").innerHTML = generadorExcusa();
+  });
+};
+
+let generadorExcusa = () => {
+  let pronombre = ["Tu", "Mi", "El", "Aquel"];
+  let sujeto = ["Perro", "Gato", "Vecino", "Profesor"];
+  let accion = ["se comió mi", "se robó mi", "rompió mi", "lanzó mi"];
+  let objeto = ["Tarea", "Celular", "Llave", "Audifono"];
+  let lugar = [
+    "en el colegio.",
+    "en el parque.",
+    "en el concierto.",
+    "en el avion."
+  ];
+
+  let pronombreIndex = Math.floor(Math.random() * 4);
+  let sujetoIndex = Math.floor(Math.random() * 4);
+  let accionIndex = Math.floor(Math.random() * 4);
+  let objetoIndex = Math.floor(Math.random() * 4);
+  let lugarIndex = Math.floor(Math.random() * 4);
+
+  return (
+    pronombre[pronombreIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    objeto[objetoIndex] +
+    " " +
+    lugar[lugarIndex]
+  );
 };
